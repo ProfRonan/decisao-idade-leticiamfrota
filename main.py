19924778946f11016ec56ada824e7af64eee17ba
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:06 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 numero = int(input())
 
 if numero < 0:
-    print('impossível!')
-    print('não precisa se alistar.')
+    print('Impossível!')
+    print('Não precisa se alistar.')
 elif numero < 18:
-    print('não precisa se alistar.')
+    print('Não precisa se alistar.')
 elif numero > 18 and numero < 65:
     print('Não esqueça de votar na próxima eleição.')
 elif numero > 65:
     print('Vá descansar.')
 else:
-    print('eita!')
+    print('Eita!')
